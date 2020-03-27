bdaebd4b55b45f17f068b2eda213dbba723415b2
Nelson Gonzalez <nelglezfl@gmail.com>
2020-03-27 09:28:01 -0400

    Initial Commit

---
+++ MoodDiary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoodDiary
+//
+//  Created by Nelson Gonzalez on 3/27/20.
+//  Copyright © 2020 Nelson Gonzalez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
